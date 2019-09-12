ad3190ad7ba9990fc1152049d574c61436b0b459
chf_git <625165473@qq.com>
2019-09-12 11:46:01 +0800

    Initial commit

---
+++ script/test_jenkens.py
@@ -0,0 +1,12 @@
+class TestJenkens():
+
+    def test001(self):
+        print("test001执行")
+
+    def test002(self):
+        print("test002执行")
+
+    def test003(self):
+        print("test003执行")
+        assert 0
+
